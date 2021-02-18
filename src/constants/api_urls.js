d047cff927a73426b7676633cdc1284c6043ac06
ron <ronisarmont@yahoo.es>
2021-02-17 21:48:11 -0400

    Update api_urls.js
    https changed

--- src/constants/api_urls.js
+++ src/constants/api_urls.js
@@ -1,4 +1,4 @@
 // const location = "Buenos Aires,ar";
 export const api_key = "ed2b42d5d4f13ff07046ba9a40fdae8a";
 export const url_base_weather =
-  "http://api.openweathermap.org/data/2.5/weather";
+  "https://api.openweathermap.org/data/2.5/weather";
